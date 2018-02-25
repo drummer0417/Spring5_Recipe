1cf94f717de60930e3559e4aad67781851485d0a
drummer0417 <hans@androidappfactory.nl>
2018-02-25 15:30:54 +0100

    Test push from IntelliJ

--- src/main/java/nl/androidappfactory/recipe/services/RecipeServiceImpl.java
+++ src/main/java/nl/androidappfactory/recipe/services/RecipeServiceImpl.java
@@ -1,5 +1,10 @@
 package nl.androidappfactory.recipe.services;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.Buffer;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,8 +40,38 @@ public class RecipeServiceImpl implements RecipeService {
 		log.debug("in: getAllRecipes()");
 		List<Recipe> recipes = (List<Recipe>) recipeRepository.findAllByOrderByDescription();
 
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File("/path")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		String S = "{\n" +
+				"  \"name\": \"hans\",\n" +
+				"  \"address\": \"fransebaan 511\",\n" +
+				"  \"age\": \"56\"\n" +
+				"}";
+
+		//language=RegExp
+		String regExp = "^[a-zA-Z0-9_]*$";
+		//language=HTML
+		String html = "<httm>\n" +
+				"\n" +
+				"    <head>\n" +
+				"        <title>test</title>\n" +
+				"    </head>\n" +
+				"    <body>\n" +
+				"    <h1>This is the body</h1>\n" +
+				"    <div>\n" +
+				"        \n" +
+				"        <p>tekst 1</p>\n" +
+				"    </div>\n" +
+				"    </body>\n" +
+				"</httm>";
 		return recipes;
 	}
+
 
 	@Override
 	public Recipe findById(long id) {
